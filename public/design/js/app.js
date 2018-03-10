2c937452e8c9eba3c355c6aaa48a316fe8a94f00
Porow <poro@42.fr>
2018-03-10 17:22:57 +0100

    Last push before bootcamp

--- public/design/js/app.js
+++ public/design/js/app.js
@@ -2523,7 +2523,7 @@ app.ready(function(){
             ev.preventDefault();
         });
     }
-    if (currentPath == "/swip")
+    if (currentPath == "/swipe")
     {
         document.body.style.backgroundColor = "#f1f1f1";
 
--- public/design/js/chat.js
+++ public/design/js/chat.js
@@ -2,103 +2,56 @@ var $messages = $('.chat-box'),
     d, h, m,
     i = 0;
 
-$(window).load(function() {
-    $messages.mCustomScrollbar();
-    setTimeout(function() {
-        fakeMessage();
-    }, 100);
-});
+var m = ('0'+ new Date().getMinutes()).slice(-2);
+var oldm = ('0'+ new Date().getMinutes()).slice(-2);
+var boo = 1;
 
-function updateScrollbar() {
-    $messages.mCustomScrollbar("update").mCustomScrollbar('scrollTo', 'bottom', {
-        scrollInertia: 10,
-        timeout: 0
-    });
-}
 
+//Get and display timestamp
 function setDate(){
-    d = new Date()
-    if (m != d.getMinutes()) {
-        m = d.getMinutes();
-        $('<div class="timestamp">' + d.getHours() + ':' + m + '</div>').appendTo($('.message:last'));
+    d = new Date();
+    da = ('0' + d.getDate()).slice(-2);
+    mon = ('0' + (d.getMonth()+1)).slice(-2);
+    yr = d.getFullYear();
+
+    m = ('0'+d.getMinutes()).slice(-2);
+    if (m > oldm + 29 || boo === 1) {
+        if (boo === 1)
+            oldm = m;
+        boo = 0;
+        $('<div class="timestamp">' + da + '/' + mon + '/' + yr + '<br>' + d.getHours() + ':' + m + '</div>').appendTo($('.chat-box'));
     }
 }
 
-function insertMessage() {
-    msg = $('.send-msg-box').val();
-    if ($.trim(msg) == '') {
-        return false;
-    }
-    $('<div class="msg-sent">' + msg + '</div>').appendTo($('.mCSB_container')).addClass('new');
-    setDate();
-    $('.message-input').val(null);
-    updateScrollbar();
-    setTimeout(function() {
-        fakeMessage();
-    }, 1000 + (Math.random() * 20) * 100);
-}
-
-$('.message-submit').click(function() {
-    insertMessage();
-});
-
-$(window).on('keydown', function(e) {
-    if (e.which == 13) {
-        insertMessage();
-        return false;
-    }
-})
-
-var Fake = [
-    'Hi there, I\'m Fabio and you?',
-    'Nice to meet you',
-    'How are you?',
-    'Not too bad, thanks',
-    'What do you do?',
-    'That\'s awesome',
-    'Codepen is a nice place to stay',
-    'I think you\'re a nice person',
-    'Why do you think that?',
-    'Can you explain?',
-    'Anyway I\'ve gotta go now',
-    'It was a pleasure chat with you',
-    'Time to make a new codepen',
-    'Bye',
-    ':)'
-]
-
-function fakeMessage() {
-    if ($('.message-input').val() != '') {
-        return false;
-    }
-    $('<div class="message loading new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure><span></span></div>').appendTo($('.mCSB_container'));
-    updateScrollbar();
-
-    setTimeout(function() {
-        $('.message.loading').remove();
-        $('<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + Fake[i] + '</div>').appendTo($('.mCSB_container')).addClass('new');
-        setDate();
-        updateScrollbar();
-        i++;
-    }, 1000 + (Math.random() * 20) * 100);
-
-}
-
-
-
-
-
-
-/*$('#messenger').submit(function () {
+$('#messenger').submit(function () {
     return false;
 });
 $("#message").attr("autocomplete", "off");
 
+$(window).on("blur focus", function(e) {
+    var prevType = $(this).data("prevType");
+
+    if (prevType != e.type) {   //  reduce double fire issues
+        switch (e.type) {
+            case "blur":
+                clearTitle();
+                break;
+            case "focus":
+                clearTitle();
+                break;
+        }
+    }
+
+    $(this).data("prevType", e.type);
+});
+
+//Chatting display
 $('#btn').click(function() {
     var text = $('#message').val();
     if (!text.replace(/\s/g, '').length){
         $('#message').val('');
         return;}
+    setDate();
     $('.chat-box').append('<div id="msg" class="msg-sent animated bounceInRight">' + text + '</div>');
     $('#message').val('');
     var elem = document.getElementById('chat');
@@ -106,7 +59,70 @@ $('#btn').click(function() {
     setTimeout(function () {
         var myArray = ['Coucou, tu vas bien ?', 'C\'est super ça ! Moi j\'adore Camagru', '🍕', '💅🏼', '🍑', '🙏', '🙈', '🙉', '🙊', '🍌'];
         var rand = myArray[Math.floor(Math.random() * myArray.length)];
+        if (document.hidden) {
+            $.playSound("../design/sounds/new_msg.mp3");
+            changeTitle();
+        }
         $('.chat-box').append('<div class="msg-received animated bounceInLeft"><img class="user-sent-pic" src="{{ baseUrl }} ../../users/002.jpg"><span>' + rand + '</span></div>');
         elem.scrollTop = elem.scrollHeight;
+        setDate();
         }, 1500);
-});*/
+});
+
+//Bot auto message
+/*setTimeout(function () {
+    var myArray = ['Coucou, tu vas bien ?', 'C\'est super ça ! Moi j\'adore Camagru', '🍕', '💅🏼', '🍑', '🙏', '🙈', '🙉', '🙊', '🍌'];
+    var rand = myArray[Math.floor(Math.random() * myArray.length)];
+    setDate();
+    if (document.hidden) {
+        $.playSound("../design/sounds/new_msg.mp3");
+        changeTitle();
+    }
+    $('.chat-box').append('<div class="msg-received animated bounceInLeft"><img class="user-sent-pic" src="{{ baseUrl }} ../../users/002.jpg"><span>' + rand + '</span></div>');
+    elem.scrollTop = elem.scrollHeight;
+}, 1500);*/
+
+//Clear badges notifications
+var title = $(document).prop('title');
+var notif = 1;
+var titlefix = title;
+
+function clearTitle(){
+    changeTitle;
+    $(document).prop('title', titlefix);
+    notif = 1;
+}
+
+//Adds notifications badges if tab isnt active
+function changeTitle() {
+    var title = $(document).prop('title');
+
+    if (title.indexOf(notif) === -1 && document.hidden) {
+        changeTitle;
+        $(document).prop('title', '('+ notif + ') '+titlefix);
+    }
+    else {
+        notif += 1;
+        if (document.hidden) {
+            changeTitle;
+            $(document).prop('title', '(' + notif + ') ' + titlefix);
+        }
+    }
+}
+
+//Sound playing
+(function ($) {
+    $.extend({
+        playSound: function () {
+            return $(
+                '<audio class="sound-player" autoplay="autoplay" style="display:none;">'
+                + '<source src="' + arguments[0] + '" />'
+                + '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
+                + '</audio>'
+            ).appendTo('body');
+        },
+        stopSound: function () {
+            $(".sound-player").remove();
+        }
+    });
+})(jQuery);
